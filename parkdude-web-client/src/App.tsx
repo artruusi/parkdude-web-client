2f874660803d7fe61a1c517f3daf93eed0bd9dbc
Teemu Pöytäniemi <teemu.poytaniemi@student.tut.fi>
2019-10-03 19:36:31 +0300

    react app created

---
+++ parkdude-web-client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div >
+      <p>parkdude-web-client works</p>
+    </div>
+  );
+}
+
+export default App;
