9124102999fab8e7dc0d5820291ee7c917c93e53
Teemu Pöytäniemi <teemu.poytaniemi@student.tut.fi>
2019-10-08 16:16:53 +0300

    jets, router and enzyme addded to the project, dummy login added

--- parkdude-web-client/src/App.tsx
+++ parkdude-web-client/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import LogIn from './components/LogIn/LogIn';
 
 const App: React.FC = () => {
+  
   return (
     <div >
       <p>parkdude-web-client works</p>
+      <LogIn></LogIn>
     </div>
+   
   );
 }
 
